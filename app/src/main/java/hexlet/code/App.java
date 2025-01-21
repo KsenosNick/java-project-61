d5ab4f8f0641d000ab887fddcf7462c82f1ff605
Николай Малютин <xenos-nick@mail.ru>
2025-01-21 20:09:15 +0300

    feat: Add game selection

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,30 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.getByName();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                0 - Exit
+                """);
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        System.out.println("Your choice: " + choice);
+
+        switch (choice) {
+            case 1:
+                Cli.getByName();
+                break;
+            case 0:
+                System.out.println("You're out of brain games");
+                break;
+            default:
+                System.out.println("Enter the game number from the suggested list");
+
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Cli {
     public static void getByName() {
+        System.out.println("Welcome to the Brain Games!");
         Scanner sc = new Scanner(System.in);
         System.out.println("May I have your name? ");
         String userName = sc.nextLine();
